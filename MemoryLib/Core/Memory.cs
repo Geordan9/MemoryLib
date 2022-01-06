74cc71d5beccf4e088e5d07d13238032b22883c4
Geordan Hanson <geo3125@gmail.com>
2022-01-06 09:23:25 -0700

    Add project files.

---
+++ MemoryLib/Core/Memory.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace MemoryLib.Core;
+
+public class Memory
+{
+    [Flags]
+    public enum ProcessAccessType
+    {
+        PROCESS_TERMINATE = 1,
+        PROCESS_CREATE_THREAD = 2,
+        PROCESS_SET_SESSIONID = 4,
+        PROCESS_VM_OPERATION = 8,
+        PROCESS_VM_READ = 16,
+        PROCESS_VM_WRITE = 32,
+        PROCESS_DUP_HANDLE = 64,
+        PROCESS_CREATE_PROCESS = 128,
+        PROCESS_SET_QUOTA = 256,
+        PROCESS_SET_INFORMATION = 512,
+        PROCESS_QUERY_INFORMATION = 1024
+    }
+
+    private IntPtr m_hProcess = IntPtr.Zero;
+
+    public Process ReadProcess { get; set; } = null;
+
+    public void Open()
+    {
+        m_hProcess = OpenProcess(0x1F0FFF, 1, (uint) ReadProcess.Id);
+    }
+
+    public void CloseHandle()
+    {
+        if (CloseHandle(m_hProcess) == 0)
+            throw new Exception("CloseHandle Failed");
+    }
+
+    public byte[] Read(IntPtr MemoryAddress, uint bytesToRead, out int bytesRead)
+    {
+        var buffer = new byte[(long) (IntPtr) bytesToRead];
+        ReadProcessMemory(m_hProcess, MemoryAddress, buffer, bytesToRead, out var lpNumberOfBytesRead);
+        bytesRead = lpNumberOfBytesRead.ToInt32();
+        return buffer;
+    }
+
+    public byte[] PointerRead(IntPtr MemoryAddress, uint bytesToRead, int[] Offset, out int bytesRead)
+    {
+        var num1 = Offset.Length;
+        bytesRead = 0;
+        var buffer1 = new byte[4];
+        var num2 = 0;
+        if (num1 == 0)
+        {
+            ReadProcessMemory(m_hProcess, MemoryAddress, buffer1, 4U, out _);
+            var num3 = ToDec(Make(buffer1)) + Offset[1];
+            var buffer2 = new byte[(long) (IntPtr) bytesToRead];
+            ReadProcessMemory(m_hProcess, (IntPtr) num3, buffer2, bytesToRead, out var lpNumberOfBytesRead);
+            bytesRead = lpNumberOfBytesRead.ToInt32();
+            return buffer2;
+        }
+
+        for (var index = 0; index <= num1; ++index)
+        {
+            if (index == num1)
+            {
+                ReadProcessMemory(m_hProcess, (IntPtr) num2, buffer1, 4U, out var lpNumberOfBytesRead);
+                bytesRead = lpNumberOfBytesRead.ToInt32();
+                return buffer1;
+            }
+
+            if (index == 0)
+            {
+                ReadProcessMemory(m_hProcess, MemoryAddress, buffer1, 4U, out _);
+                num2 = ToDec(Make(buffer1)) + Offset[index];
+            }
+            else
+            {
+                ReadProcessMemory(m_hProcess, (IntPtr) num2, buffer1, 4U, out _);
+                num2 = ToDec(Make(buffer1)) + Offset[index];
+            }
+        }
+
+        return buffer1;
+    }
+
+    public void Write(IntPtr MemoryAddress, byte[] bytesToWrite, out int bytesWritten)
+    {
+        WriteProcessMemory(m_hProcess, MemoryAddress, bytesToWrite, (uint) bytesToWrite.Length,
+            out var lpNumberOfBytesWritten);
+        bytesWritten = lpNumberOfBytesWritten.ToInt32();
+    }
+
+    public string PointerWrite(IntPtr MemoryAddress, byte[] bytesToWrite, int[] Offset, out int bytesWritten)
+    {
+        var num1 = Offset.Length;
+        bytesWritten = 0;
+        var buffer = new byte[4];
+        var Decimal1 = 0;
+        if (num1 == 0)
+        {
+            ReadProcessMemory(m_hProcess, MemoryAddress, buffer, 4U, out _);
+            var Decimal2 = ToDec(Make(buffer)) + Offset[1];
+            WriteProcessMemory(m_hProcess, (IntPtr) Decimal2, bytesToWrite, (uint) bytesToWrite.Length,
+                out var num2);
+            bytesWritten = num2.ToInt32();
+            return ToHex(Decimal2);
+        }
+
+        for (var index = 0; index <= num1; ++index)
+        {
+            if (index == num1)
+            {
+                WriteProcessMemory(m_hProcess, (IntPtr) Decimal1, bytesToWrite, (uint) bytesToWrite.Length,
+                    out var num2);
+                bytesWritten = num2.ToInt32();
+                return ToHex(Decimal1);
+            }
+
+            if (index == 0)
+            {
+                ReadProcessMemory(m_hProcess, MemoryAddress, buffer, 4U, out _);
+                Decimal1 = ToDec(Make(buffer)) + Offset[index];
+            }
+            else
+            {
+                ReadProcessMemory(m_hProcess, (IntPtr) Decimal1, buffer, 4U, out _);
+                Decimal1 = ToDec(Make(buffer)) + Offset[index];
+            }
+        }
+
+        return ToHex(Decimal1);
+    }
+
+    public int PID()
+    {
+        return ReadProcess.Id;
+    }
+
+    public string BaseAddressH()
+    {
+        return ToHex(ReadProcess.MainModule.BaseAddress.ToInt32());
+    }
+
+    public int BaseAddressD()
+    {
+        return ReadProcess.MainModule.BaseAddress.ToInt32();
+    }
+
+    public static string Make(byte[] buffer)
+    {
+        var str = "";
+        for (var index = 0; index < buffer.Length; ++index)
+            str = buffer[index].ToString("X2") + str;
+        return str;
+    }
+
+    public void Alloc(out int Addr, int Size)
+    {
+        Addr = MemoryAPI.VirtualAllocEx(m_hProcess, IntPtr.Zero, Size, MemoryAPI.AllocType.Commit,
+            MemoryAPI.Protect.ExecuteReadWrite);
+    }
+
+    public bool Dealloc(int Addr)
+    {
+        return MemoryAPI.VirtualFreeEx(m_hProcess, (IntPtr) Addr, 0, MemoryAPI.FreeType.Release);
+    }
+
+    public static string ToHex(int Decimal)
+    {
+        return Decimal.ToString("X");
+    }
+
+    public static int ToDec(string Hex)
+    {
+        return int.Parse(Hex, NumberStyles.HexNumber);
+    }
+
+    public static float ToFloat(string Hex)
+    {
+        return BitConverter.ToSingle(BitConverter.GetBytes(uint.Parse(Hex, NumberStyles.AllowHexSpecifier)), 0);
+    }
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+
+    [DllImport("kernel32.dll")]
+    public static extern int CloseHandle(IntPtr hObject);
+
+    [DllImport("kernel32.dll")]
+    public static extern int ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In] [Out] byte[] buffer,
+        uint size, out IntPtr lpNumberOfBytesRead);
+
+    [DllImport("kernel32.dll")]
+    public static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In] [Out] byte[] buffer,
+        uint size, out IntPtr lpNumberOfBytesWritten);
+}
+
+internal class MemoryAPI
+{
+    [Flags]
+    public enum AllocType
+    {
+        Commit = 0x1000,
+        Reserve = 0x2000,
+        Decommit = 0x4000
+    }
+
+    [Flags]
+    public enum FreeType
+    {
+        Decommit = 0x4000,
+        Release = 0x8000
+    }
+
+    [Flags]
+    public enum Protect
+    {
+        Execute = 0x10,
+        ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40
+    }
+
+
+    [DllImport("kernel32.dll")]
+    public static extern int VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, int dwSize,
+        AllocType flAllocationType, Protect flProtect);
+
+    [DllImport("kernel32.dll")]
+    public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, FreeType dwFreeType);
+}
---
+++ MemoryLib/Core/SigScan.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace MemoryLib.Core;
+
+public class SigScan
+{
+    /// <summary>
+    ///     m_vDumpedRegion
+    ///     The memory dumped from the external process.
+    /// </summary>
+    private byte[] m_vDumpedRegion;
+
+    /// <summary>
+    ///     ReadProcessMemory
+    ///     API import definition for ReadProcessMemory.
+    /// </summary>
+    /// <param name="hProcess">Handle to the process we want to read from.</param>
+    /// <param name="lpBaseAddress">The base address to start reading from.</param>
+    /// <param name="lpBuffer">The return buffer to write the read data to.</param>
+    /// <param name="dwSize">The size of data we wish to read.</param>
+    /// <param name="lpNumberOfBytesRead">The number of bytes successfully read.</param>
+    /// <returns></returns>
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool ReadProcessMemory(
+        IntPtr hProcess,
+        IntPtr lpBaseAddress,
+        [Out] byte[] lpBuffer,
+        int dwSize,
+        out int lpNumberOfBytesRead
+    );
+
+
+    #region "sigScan Class Construction"
+
+    /// <summary>
+    ///     SigScan
+    ///     Main class constructor that uses no params.
+    ///     Simply initializes the class properties and
+    ///     expects the user to set them later.
+    /// </summary>
+    public SigScan()
+    {
+        Process = null;
+        Address = IntPtr.Zero;
+        Size = 0;
+        m_vDumpedRegion = null;
+    }
+
+    /// <summary>
+    ///     SigScan
+    ///     Overloaded class constructor that sets the class
+    ///     properties during construction.
+    /// </summary>
+    /// <param name="proc">The process to dump the memory from.</param>
+    /// <param name="addr">The started address to begin the dump.</param>
+    /// <param name="size">The size of the dump.</param>
+    public SigScan(Process proc, IntPtr addr, int size)
+    {
+        Process = proc;
+        Address = addr;
+        Size = size;
+    }
+
+    /// <summary>
+    ///     SigScan
+    ///     Overloaded class constructor that sets the class
+    ///     properties during construction.
+    /// </summary>
+    /// <param name="stream">The memory stream to dump the data from.</param>
+    public SigScan(Stream stream)
+    {
+        Stream = stream;
+        Address = IntPtr.Zero;
+        Size = (int) stream.Length;
+    }
+
+    public SigScan(Stream stream, IntPtr addr)
+    {
+        Stream = stream;
+        Address = addr;
+        Size = (int) stream.Length;
+    }
+
+    #endregion
+
+    #region "sigScan Class Private Methods"
+
+    /// <summary>
+    ///     DumpMemory
+    ///     Internal memory dump function that uses the set class
+    ///     properties to dump a memory region.
+    /// </summary>
+    /// <returns>Boolean based on RPM results and valid properties.</returns>
+    private bool DumpMemory()
+    {
+        try
+        {
+            var usingStream = Stream != null;
+            // Checks to ensure we have valid data.
+            if (!usingStream)
+            {
+                if (Process == null)
+                    return false;
+                if (Process.HasExited)
+                    return false;
+                if (Address == IntPtr.Zero)
+                    return false;
+            }
+            else if (!(Stream.CanRead && Stream.CanSeek && Stream.CanWrite))
+            {
+                return false;
+            }
+
+            if (Size == 0)
+                return false;
+
+            // Create the region space to dump into.
+            m_vDumpedRegion = new byte[Size];
+
+
+            // Dump the memory.
+            var ret = false;
+            var nBytesRead = 0;
+            if (usingStream)
+            {
+                var origPos = Stream.Position;
+                try
+                {
+                    Stream.Position += (long) Address;
+                    nBytesRead = Stream.Read(m_vDumpedRegion, 0, Size);
+
+                    ret = true;
+                }
+                catch
+                {
+                    ret = false;
+                }
+
+                Stream.Position = origPos;
+            }
+            else
+            {
+                ret = ReadProcessMemory(Process.Handle, Address, m_vDumpedRegion, Size, out nBytesRead);
+            }
+
+            if (!ret)
+                m_vDumpedRegion = null;
+
+            // Validation checks.
+            return ret && nBytesRead == Size;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///     MaskCheck
+    ///     Compares the current pattern byte to the current memory dump
+    ///     byte to check for a match. Uses wildcards to skip bytes that
+    ///     are deemed unneeded in the compares.
+    /// </summary>
+    /// <param name="nOffset">Offset in the dump to start at.</param>
+    /// <param name="btPattern">Pattern to scan for.</param>
+    /// <param name="strMask">Mask to compare against.</param>
+    /// <returns>Boolean depending on if the pattern was found.</returns>
+    private bool MaskCheck(long nOffset, IEnumerable<byte> btPattern, string strMask)
+    {
+        // Loop the pattern and compare to the mask and dump.
+        return !btPattern
+            .Where((t, x) => strMask[x] != '?' && strMask[x] == 'x' && t != m_vDumpedRegion[nOffset + x]).Any();
+
+        // The loop was successful so we found the pattern.
+    }
+
+    #endregion
+
+    #region "sigScan Class Public Methods"
+
+    /// <summary>
+    ///     FindPattern
+    ///     Attempts to locate the given pattern inside the dumped memory region
+    ///     compared against the given mask. If the pattern is found, the offset
+    ///     is added to the located address and returned to the user.
+    /// </summary>
+    /// <param name="btPattern">Byte pattern to look for in the dumped region.</param>
+    /// <param name="strMask">The mask string to compare against.</param>
+    /// <param name="nOffset">The offset added to the result address.</param>
+    /// <returns>IntPtr - zero if not found, address if found.</returns>
+    public IntPtr FindPattern(byte[] btPattern)
+    {
+        return FindPattern(btPattern, new string('x', btPattern.Length), 0, 0, 0);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, string strMask)
+    {
+        return FindPattern(btPattern, strMask, 0, 0, 0);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, string strMask, long startAddress)
+    {
+        return FindPattern(btPattern, strMask, startAddress, 0, 0);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, string strMask, long startAddress, long endAddress)
+    {
+        return FindPattern(btPattern, strMask, startAddress, endAddress, 0);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, long startAddress)
+    {
+        return FindPattern(btPattern, new string('x', btPattern.Length), startAddress, 0, 0);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, long startAddress, long length)
+    {
+        return FindPattern(btPattern, new string('x', btPattern.Length), startAddress, length, 0);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, long startAddress, int nOffset)
+    {
+        return FindPattern(btPattern, new string('x', btPattern.Length), startAddress, 0, nOffset);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, long startAddress, long length, int nOffset)
+    {
+        return FindPattern(btPattern, new string('x', btPattern.Length), startAddress, length, nOffset);
+    }
+
+    public IntPtr FindPattern(byte[] btPattern, string strMask, long startAddress, long length, int nOffset)
+    {
+        try
+        {
+            // Dump the memory region if we have not dumped it yet.
+            if (m_vDumpedRegion == null || m_vDumpedRegion.Length == 0)
+                if (!DumpMemory())
+                    return IntPtr.Zero;
+
+            // Ensure the mask and pattern lengths match.
+            if (strMask.Length != btPattern.Length)
+                return IntPtr.Zero;
+
+            startAddress = startAddress > 0 ? startAddress : (long) Address;
+
+            var start = startAddress - (long) Address;
+
+            var end = length > 0 ? length : m_vDumpedRegion.Length;
+
+            // Loop the region and look for the pattern.
+            for (var x = start; x < end; x++)
+                if (MaskCheck(x, btPattern, strMask))
+                    // The pattern was found, return it.
+                    return new IntPtr((long) Address + x + nOffset);
+
+            // Pattern was not found.
+            return IntPtr.Zero;
+        }
+        catch (Exception)
+        {
+            return IntPtr.Zero;
+        }
+    }
+
+    /// <summary>
+    ///     ResetRegion
+    ///     Resets the memory dump array to nothing to allow
+    ///     the class to redump the memory.
+    /// </summary>
+    public void ResetRegion()
+    {
+        m_vDumpedRegion = null;
+    }
+
+    #endregion
+
+    #region "sigScan Class Properties"
+
+    /// <summary>
+    ///     m_vProcess
+    ///     The process we want to read the memory of.
+    /// </summary>
+    public Process Process { get; set; }
+
+    /// <summary>
+    ///     m_vAddress
+    ///     The starting address we want to begin reading at.
+    /// </summary>
+    public IntPtr Address { get; set; }
+
+    /// <summary>
+    ///     m_vSize
+    ///     The number of bytes we wish to read from the process.
+    /// </summary>
+    public int Size { get; set; }
+
+    /// <summary>
+    ///     m_vStream
+    ///     The stream we want to read the data of.
+    /// </summary>
+    public Stream Stream { get; set; }
+
+    #endregion
+}
